d883b0c066a4f57760c986dfbaf9cd5cfd49ee72
QuietClickCode <2572417548@qq.com>
2019-07-13 15:30:59 +0800

    初始化

---
+++ src/components/first.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="first">
+    <h1>first</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'first'
+}
+</script>
+
+<style>
+#first {
+  color:red;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,22 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import App from './App'
+import router from './router'
+import first from '@/components/first'
+
+Vue.use(ElementUI)
+Vue.use(first)
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App
+  },
+  template: '<App/>'
+})
