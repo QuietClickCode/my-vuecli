ed23b9eb0a941a4a8e3a77c149d634505a119b28
zjjlovetj <2572417548@qq.com>
2019-10-22 22:19:28 +0800

    去掉多余代码

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,12 @@
   <div id="app">
     <img src="./assets/logo.png">
     <router-view/>
-
   </div>
 </template>
 
 <script>
-import first from '@/components/first'
 export default {
-  name: 'App',
-  components: {first}
+  name: 'App'
 }
 </script>
 
