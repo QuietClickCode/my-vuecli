fcb098a9b4854fe2171b98ec7b1dfb6310c99c53
zjjlovetj <2572417548@qq.com>
2019-10-26 14:54:57 +0800

    editor完成

--- src/components/editor.vue
+++ src/components/editor.vue
@@ -1,28 +1,56 @@
 <template>
     <div id="editor">
-        <h1>我是编辑器</h1>
+        <h3 style="text-align: center">标题</h3>
         <el-row>
-            <quill-editor v-model="content"
+            <quill-editor id="title" v-model="title"
                           :options="editorOption"
                           @blur="onEditorBlur($event)"
                           @focus="onEditorFocus($event)"
                           @change="onEditorChange($event)">
             </quill-editor>
         </el-row>
+        <h3 style="text-align: center;padding-top: 50px">正文</h3>
+        <el-row>
+            <quill-editor id="content" v-model="content"
+                          :options="editorOption"
+                          @blur="onEditorBlur($event)"
+                          @focus="onEditorFocus($event)"
+                          @change="onEditorChange($event)">
+            </quill-editor>
+        </el-row>
+        <div style="padding-top: 50px;text-align: center">
+            <el-button @click="save">保存</el-button>
+        </div>
     </div>
 
 </template>
 <script>
-
+import axios from 'axios'
 export default {
     name: 'editor',
     data: function () {
         return {
+            title: '',
             content: '',
             editorOption: {}
         }
     },
     methods: {
+        save: function () {
+            var vueThis = this
+            axios({
+                url: '/saveeditor',
+                method: 'post',
+                data: { 'title': vueThis.title, 'content': vueThis.content },
+            })
+                .then(function (response) {
+                    vueThis.$message(response.data)
+                })
+                .catch(function (error) {
+                    console.log('--------------------')
+                    console.log(error)
+                })
+        },
         onEditorBlur (editor) { // 失去焦点事件
         },
         onEditorFocus (editor) { // 获得焦点事件
@@ -36,7 +64,11 @@ export default {
 </script>
 
 <style>
-.quill-editor {
-    height: 350px;
+#title {
+    height: 50px;
+}
+
+#content {
+    height: 500px;
 }
 </style>
