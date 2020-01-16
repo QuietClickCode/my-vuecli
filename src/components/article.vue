f182e71ca2879d69d0870391ab476fd1346672b6
QuietClickCode <2572417548@qq.com>
2020-01-16 10:51:15 +0800

    富文本显示爬虫爬下来的文章效果并没有el-input好

--- src/components/article.vue
+++ src/components/article.vue
@@ -75,8 +75,17 @@
                 </el-table-column>
             </el-table>
             <div class="content">
-                <el-input v-model="articledetail.content" type="textarea" autosize readonly
-                          style="color: #C7EDCC;"></el-input>
+                <!--//以前的显示文本框-->
+                 <el-input v-model="articledetail.content" type="textarea" autosize readonly
+                           style="color: #C7EDCC;"></el-input>
+                <!--富文本编辑器-->
+                <!--<quill-editor
+                    v-model="articledetail.content"
+                    ref="myQuillEditor"
+                    :options="editorOption"
+                    @blur="onEditorBlur($event)" @focus="onEditorFocus($event)"
+                    @change="onEditorChange($event)">
+                </quill-editor>-->
             </div>
 
             <div style="display: block;text-align: center">
@@ -90,211 +99,221 @@
 
 </template>
 <script>
-import axios from 'axios'
-import $ from 'jquery'
-
-var store = {
-    articledetail: '',
-    article: [],
-    IsShow: false,
-    platform: 'article',
-    id: '',
-    title: '',
-    rowstyle: {
-        height: 50
-    },
-    date: ''
-}
-export default {
-    name: 'ainotelist',
-    data () {
-        return store
-    },
-    created: function () {
-        $('body').css('background-color', '#C7EDCC')
-        if (this.$route.query.id != null && this.$route.query.platform != null) {
-            this.id = this.$route.query.id
-            this.platform = this.$route.query.platform
-            this.queryArticleById(this.id, this.platform)
-        } else {
-            /*不加动画或者,时间太少,都回补了顶部,这个速度可以滑到顶部,并且不影响后面的手动下滑*/
-            $('html,body').animate({ scrollTop: 0 }, 500)
-            /*this.toggleFullScreen();*/
-
-            this.queryArticle()
-
-        }
-    },
-    mounted () {
-        $('#table').hide()
-        $('.el-textarea__inner').css('background', '#C7EDCC')
-        $('.el-input__inner').css('background', '#C7EDCC')
-        let _this = this // 声明一个变量指向Vue实例this，保证作用域一致
-        this.timer = setInterval(() => {
-            _this.date = new Date() // 修改数据date
-        }, 1000)
-    },
-    beforeDestroy () {
-        //清楚样式
-        $('body').css('background-color', 'white')
-        $('.el-textarea__inner').css('background', 'white')
-        $('.el-input__inner').css('background', 'white')
-        if (this.timer) {
-            clearInterval(this.timer) // 在Vue实例销毁前，清除我们的定时器
-        }
-    },
-    methods: {
-        closetable: function () {
-            if ($('#table').css('display') == 'none') {
-                $('#table').show()
-            } else {
-                $('#table').hide()
-            }
+    import axios from 'axios'
+    import $ from 'jquery'
+    import { quillEditor } from 'vue-quill-editor'
+    var store = {
+        editor: null,
+        editorContent: '',
+        articledetail: '',
+        article: [],
+        IsShow: false,
+        platform: 'article',
+        id: '',
+        title: '',
+        rowstyle: {
+            height: 50
         },
-        view: function (id) {
-            var result = store.article.filter(item => item.id == id)
-            store.articledetail = result[0]
-            $('#table').hide()
-            this.id = id
-
-        },
-        next: function () {
-            store.id++
-            this.queryArticle()
-        },
-        back: function () {
-            store.id--
-            if (store.id <= 1) {
-                store.id = 1
-            }
-            this.queryArticle()
-        },
-        queryArticle: function () {
-            var vueThis = this
-            axios({
-                url: '/api' + '/queryarticle',
-                method: 'post',
-                data: {
-                    'id': store.id,
-                    'keyword': store.keyword,
-                    'title': store.title,
-                    'content': store.content,
-                    'platform': store.platform
-                },
-            })
-                .then(function (response) {
-                    store.article = response.data
-                    if (store.article.length == 1) {
-                        store.articledetail = store.article[0]
-                        $('#table').hide()
-                    } else {
-                        $('#table').show()
-                    }
-                })
-                .catch(function (error) {
-                    console.log('--------------------')
-                    console.log(error)
-                })
-        },
-        queryArticleById: function (id, platform) {
-            var vueThis = this
-            axios({
-                url: '/api' + '/queryarticle',
-                method: 'post',
-                data: {
-                    'id': id,
-                    'keyword': store.keyword,
-                    'title': store.title,
-                    'content': store.content,
-                    'platform': platform
-                },
-            })
-                .then(function (response) {
-                    store.article = response.data
-                    if (store.article.length == 1) {
-                        store.articledetail = store.article[0]
-                        $('#table').hide()
-                    } else {
-                        $('#table').show()
-                    }
-                })
-                .catch(function (error) {
-                    console.log('--------------------')
-                    console.log(error)
-                })
-        },
-        music: function () {
-            /*window.location = "http://music.nbclass.com/"*/
-            this.IsShow = !this.IsShow
-            if (this.IsShow) {
-                $('#402192905').show()
-            } else {
-                $('#402192905').hide()
-            }
-        },
-        toggleFullScreen: function () {
-            if (!document.fullscreenElement && // alternative standard method
-                !document.mozFullScreenElement && !document.webkitFullscreenElement) {// current working methods
-                if (document.documentElement.requestFullscreen) {
-                    document.documentElement.requestFullscreen()
-                } else if (document.documentElement.mozRequestFullScreen) {
-                    document.documentElement.mozRequestFullScreen()
-                } else if (document.documentElement.webkitRequestFullscreen) {
-                    document.documentElement.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT)
-                }
-            } else {
-                if (document.cancelFullScreen) {
-                    document.cancelFullScreen()
-                } else if (document.mozCancelFullScreen) {
-                    document.mozCancelFullScreen()
-                } else if (document.webkitCancelFullScreen) {
-                    document.webkitCancelFullScreen()
-                }
-            }
-        }
-
+        date: '',
+        content:null,
+        editorOption:{}
     }
-}
+    export default {
+        name: 'ainotelist',
+        data() {
+            return store
+        },
+        created: function () {
+            $('body').css('background-color', '#C7EDCC')
+            if (this.$route.query.id != null && this.$route.query.platform != null) {
+                this.id = this.$route.query.id
+                this.platform = this.$route.query.platform
+                this.queryArticleById(this.id, this.platform)
+            } else {
+                /*不加动画或者,时间太少,都回补了顶部,这个速度可以滑到顶部,并且不影响后面的手动下滑*/
+                $('html,body').animate({scrollTop: 0}, 500)
+                /*this.toggleFullScreen();*/
+
+                this.queryArticle()
+
+            }
+        },
+        mounted() {
+            $('#table').hide()
+            $('.el-textarea__inner').css('background', '#C7EDCC')
+            $('.el-input__inner').css('background', '#C7EDCC')
+            let _this = this // 声明一个变量指向Vue实例this，保证作用域一致
+            this.timer = setInterval(() => {
+                _this.date = new Date() // 修改数据date
+            }, 1000)
+        },
+        beforeDestroy() {
+            //清楚样式
+            $('body').css('background-color', 'white')
+            $('.el-textarea__inner').css('background', 'white')
+            $('.el-input__inner').css('background', 'white')
+            if (this.timer) {
+                clearInterval(this.timer) // 在Vue实例销毁前，清除我们的定时器
+            }
+        },
+        methods: {
+            onEditorBlur(){//失去焦点事件
+            },
+            onEditorFocus(){//获得焦点事件
+            },
+            onEditorChange(){//内容改变事件
+            },
+            closetable: function () {
+                if ($('#table').css('display') == 'none') {
+                    $('#table').show()
+                } else {
+                    $('#table').hide()
+                }
+            },
+            view: function (id) {
+                var result = store.article.filter(item => item.id == id)
+                store.articledetail = result[0]
+                $('#table').hide()
+                this.id = id
+
+            },
+            next: function () {
+                store.id++
+                this.queryArticle()
+            },
+            back: function () {
+                store.id--
+                if (store.id <= 1) {
+                    store.id = 1
+                }
+                this.queryArticle()
+            },
+            queryArticle: function () {
+                var vueThis = this
+                axios({
+                    url: '/api' + '/queryarticle',
+                    method: 'post',
+                    data: {
+                        'id': store.id,
+                        'keyword': store.keyword,
+                        'title': store.title,
+                        'content': store.content,
+                        'platform': store.platform
+                    },
+                })
+                    .then(function (response) {
+                        store.article = response.data
+                        if (store.article.length == 1) {
+                            store.articledetail = store.article[0]
+                            $('#table').hide()
+                        } else {
+                            $('#table').show()
+                        }
+                    })
+                    .catch(function (error) {
+                        console.log('--------------------')
+                        console.log(error)
+                    })
+            },
+            queryArticleById: function (id, platform) {
+                var vueThis = this
+                axios({
+                    url: '/api' + '/queryarticle',
+                    method: 'post',
+                    data: {
+                        'id': id,
+                        'keyword': store.keyword,
+                        'title': store.title,
+                        'content': store.content,
+                        'platform': platform
+                    },
+                })
+                    .then(function (response) {
+                        store.article = response.data
+                        if (store.article.length == 1) {
+                            store.articledetail = store.article[0]
+                            $('#table').hide()
+                        } else {
+                            $('#table').show()
+                        }
+                    })
+                    .catch(function (error) {
+                        console.log('--------------------')
+                        console.log(error)
+                    })
+            },
+            music: function () {
+                /*window.location = "http://music.nbclass.com/"*/
+                this.IsShow = !this.IsShow
+                if (this.IsShow) {
+                    $('#402192905').show()
+                } else {
+                    $('#402192905').hide()
+                }
+            },
+            toggleFullScreen: function () {
+                if (!document.fullscreenElement && // alternative standard method
+                    !document.mozFullScreenElement && !document.webkitFullscreenElement) {// current working methods
+                    if (document.documentElement.requestFullscreen) {
+                        document.documentElement.requestFullscreen()
+                    } else if (document.documentElement.mozRequestFullScreen) {
+                        document.documentElement.mozRequestFullScreen()
+                    } else if (document.documentElement.webkitRequestFullscreen) {
+                        document.documentElement.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT)
+                    }
+                } else {
+                    if (document.cancelFullScreen) {
+                        document.cancelFullScreen()
+                    } else if (document.mozCancelFullScreen) {
+                        document.mozCancelFullScreen()
+                    } else if (document.webkitCancelFullScreen) {
+                        document.webkitCancelFullScreen()
+                    }
+                }
+            }
+
+        }
+    }
 </script>
 
 <style>
-@import "../assets/css/font-awesome-4.7.0/css/font-awesome.min.css";
-@import "../assets/css/APlayer.min.css";
+    @import "../assets/css/font-awesome-4.7.0/css/font-awesome.min.css";
+    @import "../assets/css/APlayer.min.css";
 
-/*中间内容设置上下间距*/
-.content {
-    margin-bottom: 35px;
-}
+    /*中间内容设置上下间距*/
+    .content {
+        margin-bottom: 35px;
+    }
 
-/*文本域,护眼绿*/
-.el-textarea__inner {
-    background: #C7EDCC;
-}
+    /*文本域,护眼绿*/
+    .el-textarea__inner {
+        background: #C7EDCC;
+    }
 
-/*输入框,护眼绿*/
-/*.el-input__inner {
-    background: #C7EDCC;
-    border: 1px solid #409EFF;
-    height: 35px;
-}*/
+    /*输入框,护眼绿*/
+    /*.el-input__inner {
+        background: #C7EDCC;
+        border: 1px solid #409EFF;
+        height: 35px;
+    }*/
 
-/*改变placeholder颜色*/
-.el-input__inner::-webkit-input-placeholder {
-    /* WebKit browsers */
-    color: gray;
-}
+    /*改变placeholder颜色*/
+    .el-input__inner::-webkit-input-placeholder {
+        /* WebKit browsers */
+        color: gray;
+    }
 
-/*网上找的方法实现,高度控制,最小41px*/
-/*https://www.cnblogs.com/secretAngel/p/10241791.html*/
-.el-table__header tr,
-.el-table__header th {
-    padding: 0;
-    height: 20px;
-}
+    /*网上找的方法实现,高度控制,最小41px*/
+    /*https://www.cnblogs.com/secretAngel/p/10241791.html*/
+    .el-table__header tr,
+    .el-table__header th {
+        padding: 0;
+        height: 20px;
+    }
 
-.el-table__body tr,
-.el-table__body td {
-    padding: 0;
-    height: 20px;
-}
+    .el-table__body tr,
+    .el-table__body td {
+        padding: 0;
+        height: 20px;
+    }
 </style>
