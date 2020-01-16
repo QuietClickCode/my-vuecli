ed6bf759d34d9189748f756cc06f4d347c694d09
QuietClickCode <2572417548@qq.com>
2020-01-16 13:56:12 +0800

    添加live2d看板娘https://huaji8.top/post/live2d-plugin-2.0/

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,28 @@
 <template>
-  <div id="app">
-    <router-view/>
-  </div>
+    <div id="app">
+        <router-view/>
+    </div>
 </template>
 
 <script>
-export default {
-  name: 'App',
-}
+    export default {
+        name: 'App',
+        created() {
+            setTimeout(() => {
+                window.L2Dwidget.init({
+                    pluginRootPath: 'static/live2dw/',
+                    pluginJsPath: 'lib/',
+                    pluginModelPath: 'live2d-widget-model-koharu/assets/',
+                    tagMode: false,
+                    debug: false,
+                    model: {jsonPath: '/static/live2dw/live2d-widget-model-koharu/assets/koharu.model.json'},
+                    display: {position: 'right', width: 150, height: 300},
+                    mobile: {show: true},
+                    log: false
+                })
+            }, 1000)
+        }
+    }
 </script>
 
 <style>
