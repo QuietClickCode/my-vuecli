5a8e48ff34a223d236e644395eb83312e0e62708
QuietClickCode <2572417548@qq.com>
2020-01-17 19:54:54 +0800

    测试jenkins自动部署,成功

--- src/App.vue
+++ src/App.vue
@@ -17,7 +17,7 @@
                     tagMode: false,
                     debug: false,
                     model: {jsonPath: '/static/live2dw/live2d-widget-model-koharu/assets/koharu.model.json'},
-                    display: {position: 'left', width: 150, height: 300},
+                    display: {position: 'right', width: 150, height: 300},
                     mobile: {show: true},
                     log: false
                 })
