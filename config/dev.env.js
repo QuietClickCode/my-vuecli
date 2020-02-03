fd0f9b2bbeecb7c41f20a4a390f1df262e0aaed5
QuietClickCode <2572417548@qq.com>
2020-02-04 00:02:28 +0800

    测试是否能通过NODE_ENV 区分开发和测试环境之前

--- config/dev.env.js
+++ config/dev.env.js
@@ -1,8 +1,8 @@
 'use strict'
 const merge = require('webpack-merge')
-const prodEnv = require('./prod.env')
+const devEnv = require('./dev.env')
 
-module.exports = merge(prodEnv, {
+module.exports = merge(devEnv, {
     NODE_ENV: '"development"',
     HOST: '"http://114.55.94.186:80"'
 })
--- config/prod.env.js
+++ config/prod.env.js
@@ -2,6 +2,6 @@
 const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
 module.exports = merge(prodEnv, {
-    NODE_ENV: '"development"',
+    NODE_ENV: '"production"',
     HOST: '"http://114.55.94.186:80"'
 })
