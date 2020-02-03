269f345c6c6beeeb671b9c8173113addfbd82115
QuietClickCode <2572417548@qq.com>
2020-02-04 02:11:22 +0800

    不能用localhost

--- config/prod.env.js
+++ config/prod.env.js
@@ -3,5 +3,5 @@ const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
 module.exports = merge(prodEnv, {
     NODE_ENV: '"production"',
-    HOST: '"https://localhost"'
+    HOST: '"https://114.55.94.186"'
 })
