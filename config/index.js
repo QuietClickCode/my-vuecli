51c525128aacd32abdfe91032acae6371416637c
QuietClickCode <2572417548@qq.com>
2020-02-15 14:55:43 +0800

    本地端口和服务器端口一致8888

--- config/index.js
+++ config/index.js
@@ -23,7 +23,7 @@ module.exports = {
 
         // Various Dev Server settings
         host: 'localhost', // can be overwritten by process.env.HOST
-        port: 80, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
+        port: 8888, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
         autoOpenBrowser: true,
         errorOverlay: true,
         notifyOnErrors: true,
